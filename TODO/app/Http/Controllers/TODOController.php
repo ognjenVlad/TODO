e5c2537f9b0b50d263745c1e44e15b228da18be0
achy21 <ogauzz1@gmail.com>
2018-06-22 16:40:10 +0200

    pozvean kontrolere uradjeno dobavljanje svih entiteta iz baze:

---
+++ TODO/app/Http/Controllers/TODOController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Task;
+class TODOController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json([
+            'tasks' =>Task::all()->jsonSerialize()
+        ], 200);
+
+    }
+    public function show(Task $task)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Task $task)
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    protected function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $name)
+    {
+        $task = Task::where('name', '=', $name)->first();
+
+        // change the attribute
+        $task->text = $request->text;
+        //$task->priority = $request->priority;
+        $task->save();
+        return response()->json([
+            'message' => 'Updated'
+        ], 200);
+    }
+    public function create()
+    {
+        $task = Task::create([
+            'name' => $request->name,
+            'text' => $request->text,
+            'priority' => $request->priority,
+        ]);
+        return response()->json([
+            'task' => $task,
+            'message' => 'Success'
+        ], 200);
+    }
+
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($name)
+    {
+        $task = Task::findOrFail($name);
+        $task->delete();
+
+        return response()->json([
+            'message' => 'Task deleted successfully!'
+        ], 200);
+
+    }
+}
